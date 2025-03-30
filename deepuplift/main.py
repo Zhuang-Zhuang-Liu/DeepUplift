1fc6d5fc76046c97883dea91d5bdc261d76095ca
ZHUANGZHUANG_LIU <59521697+Zhuang-Zhuang-Liu@users.noreply.github.com>
2025-03-30 14:21:39 +0800

    Add files via upload

---
+++ deepuplift/main.py
@@ -0,0 +1,106 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from functools import partial
+from utils.evaluate import *
+
+from trainer import Trainer
+from models.DragonNet import *
+from models.DragonDeepFM import *
+from models.EFIN import *
+
+
+if __name__ == "__main__":
+
+  # define
+  treatment = 'treatment'
+  outcome = 'visit'   
+  features = ['f0', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11']
+
+  # io
+  csv_name = r'/Users/maczhuangzhuang/Documents/DeepUplift/dataset/criteo-uplift-v2.1-50w.csv'
+  df = pd.read_csv(csv_name).head(200000)
+  group_stats = df.groupby(treatment).agg({outcome: ['mean', 'count','sum']})
+  print('整体:',group_stats)
+
+
+  # Split 
+  df_train, df_test = train_test_split(df, test_size=0.3, random_state=42)  
+  print(df_train.shape, df_test.shape)
+  # 统计训练集和测试集的按treatment分组后的outcome的总数和均值
+  train_stats = df_train.groupby(treatment).agg({outcome: ['mean', 'count','sum']})
+  test_stats = df_test.groupby(treatment).agg({outcome: ['mean', 'count','sum']})
+  print("训练集统计结果:",train_stats,"\n测试集统计结果:",test_stats)
+
+  
+  X_train,Y_train,T_train = df_train[features],df_train[outcome],df_train[treatment]
+  X_test,Y_test,T_test = df_test[features],df_test[outcome],df_test[treatment]
+
+
+  # choose model
+  est1 = Trainer(model = DragonNet(len(features)),
+                 epochs=2,batch_size=64,
+                 loss_f = partial(dragonnet_loss, alpha=1.0, beta=1.0,tarreg=True) 
+                )
+  est2 = Trainer(model = DragonNetDeepFM(input_dim=12,num_continuous=12,kind='classi', num_treatments=2,
+                                         embedding_size=8,shared_dim=32), 
+                 epochs=1,batch_size=64,
+                 loss_f = partial(dragon_loss, alpha=1) 
+                ) 
+  est3 = Trainer(model = EFIN(input_dim=12, hc_dim=16, hu_dim=8, is_self=True, act_type="elu") , 
+                  epochs=2,batch_size= 64,  
+                  loss_f = partial(efin_loss)  
+                  )  
+
+  model = est1
+  model.fit(X_train, Y_train, T_train,valid_perc=0.2)
+  t_pred,y_preds, *_ = model.predict(X_test,T_test)
+
+  # predict - validation
+  df_result = pd.concat([T_test, Y_test, 
+                        pd.DataFrame({'y_diff': (y_preds[1] - y_preds[0]).flatten(),
+                                      'y0_pred': y_preds[0].flatten(),
+                                      'y1_pred': y_preds[1].flatten(),
+                                      }, index=T_test.index)], axis=1)
+
+  aa = plot_bins_uplift(df_result, 
+                         uplift_col='y_diff', 
+                         treatment_col=treatment, 
+                         outcome_col=outcome,
+                         figsize=(8, 8),
+                         bins = 5,
+                         if_plot = False,
+                         )
+
+  # evaluate - uplift
+  qini, qini_scores = uplift_metric( df=df_result[[treatment,outcome,'y_diff',]],
+                                      kind='qini',
+                                      outcome_col=outcome,
+                                      treatment_col=treatment,
+                                      treatment_value = 1,
+                                      if_plot =  True,
+                                      )
+
+  auuc, auuc_scores = uplift_metric(  df=df_result[[treatment,outcome,'y_diff',]],
+                                      kind='auuc',
+                                      outcome_col=outcome,
+                                      treatment_col=treatment,
+                                      treatment_value = 1,
+                                      if_plot =  False,
+                                      )
+
+  # evaluate - response
+  res = calculate_metrics_by_treatment(df_result,outcome_col=outcome,treatment_col=treatment, threshold=0.5)
+
+  # show
+  plt.show()
+
+
+
+
+
+
+
+
+
+                     
+
---
+++ deepuplift/models/DragonNet.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DragonNet(nn.Module):
+    def __init__(self, input_dim, shared_hidden=200, outcome_hidden=100):
+        super(DragonNet, self).__init__() 
+        self.fc1 = nn.Linear(in_features=input_dim, out_features=shared_hidden)
+        self.fc2 = nn.Linear(in_features=shared_hidden, out_features=shared_hidden)
+        self.fcz = nn.Linear(in_features=shared_hidden, out_features=shared_hidden)
+
+        self.treat_out = nn.Linear(in_features=shared_hidden, out_features=1)
+
+        self.y0_fc1 = nn.Linear(in_features=shared_hidden, out_features=outcome_hidden)
+        self.y0_fc2 = nn.Linear(in_features=outcome_hidden, out_features=outcome_hidden)
+        self.y0_out = nn.Linear(in_features=outcome_hidden, out_features=1)
+
+        self.y1_fc1 = nn.Linear(in_features=shared_hidden, out_features=outcome_hidden)
+        self.y1_fc2 = nn.Linear(in_features=outcome_hidden, out_features=outcome_hidden)
+        self.y1_out = nn.Linear(in_features=outcome_hidden, out_features=1)
+
+        self.epsilon = nn.Linear(in_features=1, out_features=1)
+        torch.nn.init.xavier_normal_(self.epsilon.weight)
+        
+    def forward(self, inputs,tr=None):
+        """
+        forward method to train model.
+
+        Parameters
+        ----------
+        inputs: torch.Tensor
+            covariates
+
+        Returns
+        -------
+        y0: torch.Tensor
+            outcome under control
+        y1: torch.Tensor
+            outcome under treatment
+        t_pred: torch.Tensor
+            predicted treatment
+        eps: torch.Tensor
+            trainable epsilon parameter
+        """
+        x = F.relu(self.fc1(inputs))
+        x = F.relu(self.fc2(x))
+        z = F.relu(self.fcz(x))
+
+        t_pred = torch.sigmoid(self.treat_out(z))
+
+        y0 = F.relu(self.y0_fc1(z))
+        y0 = F.relu(self.y0_fc2(y0))
+        y0 = self.y0_out(y0)
+
+        y1 = F.relu(self.y1_fc1(z))
+        y1 = F.relu(self.y1_fc2(y1))
+        y1 = self.y1_out(y1)
+
+        eps = self.epsilon(torch.ones_like(t_pred)[:, 0:1])
+        y_preds = [y0, y1]
+        return t_pred,y_preds,eps
+
+
+def dragonnet_loss( t_pred, y_preds,t_true,y_true, eps,alpha=1.0,beta=1.0,tarreg=True):
+    """
+    Parameters
+    ----------
+    y_true: torch.Tensor
+        Actual target variable
+    t_true: torch.Tensor
+        Actual treatment variable
+    t_pred: torch.Tensor
+        Predicted treatment
+    y_preds:[torch.Tensor,torch.Tensor]
+        y0_pred: Predicted target variable under control,y1_pred: 
+    eps: torch.Tensor
+        Trainable epsilon parameter
+    alpha: float
+        loss component weighting hyperparameter between 0 and 1 （ 调节不同部分损失在整体损失计算中的相对重要性 ）
+    beta: float
+        targeted regularization hyperparameter between 0 and 1 （ 用于控制靶向正则化部分在最终损失计算中的影响力 ）
+    tarreg: bool
+        whether to use targeted regularization
+    Returns
+    -------
+    loss: torch.Tensor
+    """
+    t_pred = (t_pred + 0.01) / 1.02  # 归一化处理（ 避免出现0）
+    treatment_loss = torch.sum(F.binary_cross_entropy(t_pred, t_true))   # 倾向性loss（交叉熵）
+
+    loss0 = torch.sum((1. - t_true) * torch.square(y_true - y_preds[0]))   # 对照组loss（均方误差，MSE Mean Squared Error）
+    loss1 = torch.sum(t_true * torch.square(y_true - y_preds[1]))  # 实验组loss（均方误差，MSE Mean Squared Error）
+    outcome_loss = loss0 + loss1
+    loss = outcome_loss + alpha * treatment_loss
+
+    if tarreg:
+        y_pred = t_true * y_preds[1] + (1 - t_true) * y_preds[0]   # 按实际的t预测的y
+        # h = (t_true / t_pred) - ((1 - t_true) / (1 - t_pred))  
+        # y_pert = y_pred + eps * h  # 源码：按实际的t预测的y + 动态调整的eps参数 * h 
+        y_pert = y_pred + eps # 可渐进一致 ：2个塔的loss一起下降, 去掉eps无法渐进一致
+        targeted_regularization = torch.sum((y_true - y_pert)**2)
+        loss = loss + beta * targeted_regularization   
+
+    return loss, outcome_loss, treatment_loss 
+
+
+
+
